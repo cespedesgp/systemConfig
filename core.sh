adb84ba18f5aabe4a362d2a7b273171032765c6f
Guilherme Peña Céspedes <55372036+cespedesgp@users.noreply.github.com>
2021-09-27 21:05:31 -0400

    create core.sh
    Core packages for using without Graphical User Interface.

---
+++ core.sh
@@ -0,0 +1,14 @@
+apt install -y \
+    curl \            # ferramenta para transferência de arquivos via url (cli)
+    git \             # gerenciador de versões
+    vim \             # editor de texto
+    ntfs-3g \         # leitor/escritor de drivers NTFS para FUSE
+    unrar \           # desconpactador de vários formatos
+    alsa-utils \      # controlador de áudio alsa
+    moc \             # reprodutor de áudio
+#    blueman \         # controlador do bluetooth
+#    pulseaudio \      # pulseaudio
+#    pulseaudio-bluetooth \ # plugin do pulseaudio para bluetooth
+#    bluez-tools \          # gerenciador do bluetooth (cli)
+    intel-microcode \ # microcode intel
+#    firmwares
