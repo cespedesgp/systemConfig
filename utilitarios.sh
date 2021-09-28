18ba91bbe8b7373e709820eb27e5b3b55360c20a
Guilherme Peña Céspedes <55372036+cespedesgp@users.noreply.github.com>
2021-09-27 23:10:39 -0400

    create utilitarios.sh

---
+++ utilitarios.sh
@@ -0,0 +1,13 @@
+apt install -y \
+    firefox-esr \           # navegador de world wide web
+    libreoffice \           # pacote office libre (+ 1.1GB)
+    okular \                # leitor okular, com os pacotes abaixo,
+    okular-extra-backends \ # lê quase qualquer formato de documento
+    okular-mobile \
+    okular-backend-odt \
+    okular-backend-odp \
+    gimp \                  # editor de imagens poderoso
+    inkscape \              # editor de imagens vetoriais
+#    krita \                 # editor de imagens
+#    blender \               # editor de vídeos e editor 3D
+    obs-studio \            # estudio de gravação de vídeos
