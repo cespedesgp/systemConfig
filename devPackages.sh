850f8a2c4d24c2dc8eb4af634dc85188cd328f28
Guilherme Peña Céspedes <55372036+cespedesgp@users.noreply.github.com>
2021-09-27 21:36:50 -0400

    create devPackages.sh
    create list of packages for development

---
+++ devPackages.sh
@@ -0,0 +1,17 @@
+apt install -y \
+    build-essentials \ # conjunto de ferramentas para compilação
+    apt-listchanges \  # lista mudanças quando instalados pacotes
+    apt-listbugs \     # lista os bugs quando instalados pacotes
+#    default-jdk \      # JDK para desenvolvimento em java
+#    libssl-dev \       # biblioteca de desenvolvimento para o libssl e o libcrypto
+#    exubarant-ctags    # para criação de ctags
+#    ncurses-term \     # biblioteca para dev de interface em modo texto
+#    fontconfig \       # gerenciamento de fontes
+#    imagemagick \      # ferramenta para manipular imagens (cli)
+#    libmagickwand-dev \# biblioteca para imagemagick
+#    software-properties-common \ # abstração de gerenciamento de pacotes
+     ack-grep \        # grep otimizado
+     silversearch-ag \ # grep otimizado
+#     llvm \           # compilador llvm
+#     tcc \            # tiny c compiler
+#     make \           # utilitário para direcionar a compilação
